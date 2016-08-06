2b275f4dae97774c2928a814a90ba0ec820047e1
Mohammed Hussein <hack.mhd@gmail.com>
2016-08-06 18:30:27 +0300

    users can be created without photos

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\Role;
 use Illuminate\Http\Request;
-
+use App\User;
 use App\Http\Requests;
 
 class AdminUsersController extends Controller
@@ -15,7 +17,10 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+
+        $users = User::all();
+
+        return view('admin.users.index' , compact('users'));
     }
 
     /**
@@ -25,7 +30,10 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        //
+        // lists what will be shown then the actual value
+        $roles = Role::lists('name' , 'id')->all();
+
+        return view('admin.users.create' , compact('roles'));
     }
 
     /**
@@ -34,9 +42,15 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
+
+        User::create($request->all());
+
+        return redirect('admin/users');
+
+        //return $request->all();
     }
 
     /**
