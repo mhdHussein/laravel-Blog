37bd533bd6dc02306acd47b2f54aa52680f2d6d5
Mohammed Hussein <hack.mhd@gmail.com>
2016-08-06 12:51:29 +0300

    Admin master page setup

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,5 @@
+@include('layouts.admin')
+
+@section('content')
+    <h3>Admin</h3>
+@stop
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,7 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Back
- * Date: 05/08/16
- * Time: 05:37 م
- */ 
+hello
